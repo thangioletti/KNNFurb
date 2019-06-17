3c1ccd8ee6cf14bba8a55b68c56c7ef334a25d77
Tiago Henrique Angioletti <tiago.angioletti@ellevo.com.br>
2019-06-16 21:03:28 -0300

    Correção normalizacao

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def meuKnn(dadosTrain, rotuloTrain, dadosTeste, rotuloTeste, k):
         distanciaLinha = sorted(distanciaLinha, key=operator.itemgetter(1))
         distancia.append(distanciaLinha)
 
-     #Calcula a acuracia
+    #Calcula a acuracia
     acuracia = 0
     for i in range(len(dadosTeste)):
         rotulos = []
@@ -34,30 +34,32 @@ def meuKnn(dadosTrain, rotuloTrain, dadosTeste, rotuloTeste, k):
     acuracia = acuracia/len(dadosTrain)*100
     return acuracia
 
-def normalizacao(dados):
+def normalizacao(dadosTrain, dadosTeste):
 
-    for j in range(len(dados[0])):
+    for j in range(len(dadosTrain[0])):
         maior = 0
+        menor = 0
         #pega o maior dessa coluna
-        for i in range(len(dados)):
-            if dados[i][j] > maior:
-                maior = dados[i][j]
+        for i in range(len(dadosTrain)):
+            if dadosTrain[i][j] > maior:
+                maior = dadosTrain[i][j]
+            if dadosTrain[i][j] < menor:
+                menor = dadosTrain[i][j]
 
-        #normaliza entre valores de 0...1
-        for i in range(len(dados)):
-            dados[i][j] = dados[i][j]/maior
+        #normaliza entre valores de 0...1 o treinamento
+        for i in range(len(dadosTrain)):
+            dadosTrain[i][j] = round((dadosTrain[i][j]-menor)/(maior-menor),2)
+        for i in range(len(dadosTeste)):
+            dadosTeste[i][j] = round((dadosTeste[i][j]-menor)/(maior-menor),2)
 
-    return dados
 
-
-mat = scipy.loadmat('grupoDados2.mat')
+mat = scipy.loadmat('grupoDados1.mat')
 grupoTest  = mat['grupoTest']
 grupoTrain = mat['grupoTrain']
 trainRots  = mat['trainRots']
 testRots   = mat['testRots']
 
-grupoTest = normalizacao(grupoTest)
-grupoTrain = normalizacao(grupoTrain)
+normalizacao(grupoTrain, grupoTest)
 
 print(meuKnn(grupoTrain, trainRots, grupoTest, testRots, 1))
 print(meuKnn(grupoTrain, trainRots, grupoTest, testRots, 2))
